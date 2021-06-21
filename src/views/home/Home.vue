4c609b76ee246703e5bde869182e1e5e48628d64
tongzihao <1150227171@qq.com>
2021-06-21 11:42:40 +0800

    620

---
+++ src/views/home/Home.vue
@@ -0,0 +1,40 @@
+<template>
+<div id="home">
+  <nav-bar class="home-nav">
+    <div slot="center">购物街</div>
+  </nav-bar>
+  <home-swiper :banners="banners"></home-swiper>
+  <recommend-view :recommends="recommends"></recommend-view>
+</div>
+</template>
+
+<script>
+import NavBar from "@/components/common/navbar/NavBar";
+import HomeSwiper from "@/views/home/childComps/HomeSwiper";
+import {getHomeMultidata} from "@/network/home"
+import RecommendView from "@/views/home/childComps/RecommendView";
+export default {
+  name: "Home",
+  components: {RecommendView, HomeSwiper,NavBar},
+  data(){
+    return {
+      banners:[],
+      recommends:[]
+    }
+  },
+  created() {
+    getHomeMultidata().then(res=>{
+      console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list
+    })
+  }
+}
+</script>
+
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,33 @@
+<template>
+  <swiper>
+    <slide v-for="item in banners">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+      </a>
+    </slide>
+  </swiper>
+</template>
+
+<script>
+import Swiper from "@/components/common/swiper/Swiper";
+import Slide from "@/components/common/swiper/SwiperItem";
+export default {
+  name: "HomeSwiper",
+  props:{
+    banners:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  },
+  components:{
+    Slide,
+    Swiper
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
